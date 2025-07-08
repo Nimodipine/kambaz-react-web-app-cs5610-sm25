ed7c6d96c366435d0acd7f192117d5d1bfa8e9ea
Nimodipine <megan5775@gmail.com>
2025-07-07 23:27:18 -0700

    added other html fields

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -242,6 +242,31 @@ tag to tell browsers to render the gaps.
                 <option value="FANTASY">         Fantasy         </option>
             </select>
 
+            <h4>Other HTML field types</h4>
+
+            <label htmlFor="wd-text-fields-email"> Email: </label>
+            <input type="email"
+                    placeholder="jdoe@somewhere.com"
+                    id="wd-text-fields-email"/><br/>
+
+            <label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
+            <input type="number"
+                    value="100000"
+                    placeholder="1000"
+                    id="wd-text-fields-salary-start"/><br/>
+
+            <label htmlFor="wd-text-fields-rating"> Rating: </label>
+            <input type="range"
+                    value="4"
+                    max="5"
+                    placeholder="Doe"
+                    id="wd-text-fields-rating"/><br/>
+
+            <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+            <input type="date"
+                    value="2000-01-21"
+                   id="wd-text-fields-dob"/><br/>        
+
 
         </div>
     );
