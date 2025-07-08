cb9db03b7137dc0b45ec8701b1b9ecb6669b066e
Nimodipine <megan5775@gmail.com>
2025-07-07 23:16:40 -0700

    added forms, textareas, buttons and checkboxes to index.tsx

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -159,6 +159,67 @@ tag to tell browsers to render the gaps.
                 <br />
                 <img id="wd-teslabot" src="/images/teslabot.jpg" height="200px" /></div>
 
+            <div id="wd-forms">
+                <h4>Form Elements</h4>
+                <form id="wd-text-fields">
+                    <h5>Text Fields</h5>
+                    <label htmlFor="wd-text-fields-username">Username:</label>
+                    <input placeholder="jdoe" id="wd-text-fields-username" /> <br />
+                    <label htmlFor="wd-text-fields-password">Password:</label>
+                    <input type="password" value="123@#$asd" id="wd-text-fields-password" />
+                    <br />
+                    <label htmlFor="wd-text-fields-first-name">First name:</label>
+                    <input type="text" title="John" id="wd-text-fields-first-name" /> <br />
+                    <label htmlFor="wd-text-fields-last-name">Last name:</label>
+                    <input type="text" placeholder="Doe"
+                            value="Wonderland"
+                            title="The last name"
+                            id="wd-text-fields-last-name" />
+                    {/* copy rest of form elements here  */}
+                </form>
+            </div>
+
+            <h5>Text boxes</h5>
+            <label>Biography:</label><br/>
+            <textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+
+            <h5 id="wd-buttons">Buttons</h5>
+            <button type="button"
+                    onClick={() => alert("Life is Good!")}
+                    id="wd-all-good">
+                Hello World!
+            </button>
+
+            <h5 id="wd-radio-buttons">Radio buttons</h5>
+
+            <label>Favorite movie genre:</label><br />
+
+            <input type="radio" name="radio-genre" id="wd-radio-comedy"/>
+            <label htmlFor="wd-radio-comedy">Comedy</label><br />
+
+            <input type="radio" name="radio-genre" id="wd-radio-drama"/>
+            <label htmlFor="wd-radio-drama">Drama</label><br />
+
+            <input type="radio" name="radio-genre" id="wd-radio-scifi"/>
+            <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
+            <input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
+            <label htmlFor="wd-radio-fantasy">Fantasy</label>
+
+            <h5 id="wd-checkboxes">Checkboxes</h5>
+            <label>Favorite movie genre:</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+            <label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+            <label htmlFor="wd-chkbox-drama">Drama</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+            <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+            <label htmlFor="wd-chkbox-fantasy">Fantasy</label>  
+
         </div>
     );
 }
