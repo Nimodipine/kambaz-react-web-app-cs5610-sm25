8c0a4e108abc4d0f49d63da4987ddf3fcb0edb9a
Nimodipine <megan5775@gmail.com>
2025-07-08 08:26:11 -0700

    modified assignments and editor

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -1,23 +1,88 @@
 export default function AssignmentEditor() {
   return (
     <div id="wd-assignments-editor">
-      <label htmlFor="wd-name">Assignment Name</label>
+      <label htmlFor="wd-name" style={{ fontSize: '1.2em', fontWeight: 'bold' }}>
+      Assignment Name
+      </label><br />
       <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
-      <textarea id="wd-description">
-        The assignment is available online Submit a link to the landing page of
+
+      <label htmlFor="wd-description">Description</label><br />
+      <textarea id="wd-description" rows={4} cols={50}>
+        The assignment is available online. Submit a link to the landing page of
       </textarea>
-      <br />
+      <br /><br />
+
       <table>
-        <tr>
-        <td align="right" valign="top">
-            <label htmlFor="wd-points">Points</label>
-        </td>
-        <td>
-            <input id="wd-points" value={100} />
-        </td>
-        </tr>
-        {/* Complete on your own */}
+        <tbody>
+          <tr>
+            <td align="right"><label htmlFor="wd-points">Points</label></td>
+            <td><input id="wd-points" type="number" defaultValue={100} /></td>
+          </tr><br />
+
+          <tr>
+            <td align="right"><label htmlFor="wd-group">Assignment Group</label></td>
+            <td>
+              <select id="wd-group">
+                <option>ASSIGNMENTS</option>
+                <option>QUIZZES</option>
+                <option>EXAMS</option>
+                <option>PROJECT</option>
+              </select>
+            </td>
+          </tr><br />
+
+          <tr>
+            <td align="right"><label htmlFor="wd-display-grade-as">Display Grade as</label></td>
+            <td>
+              <select id="wd-display-grade-as">
+                <option>Percentage</option>
+                <option>Complete/Incomplete</option>
+                <option>Points</option>
+              </select>
+            </td>
+          </tr><br />
+
+          <tr>
+            <td align="right" valign="top"><label htmlFor="wd-submission-type">Submission Type</label></td>
+            <td>
+              <select id="wd-submission-type">
+                <option>Online</option>
+                <option>No Submission</option>
+              </select>
+              <br /><br />
+
+              <div>Online Entry Options
+              </div>
+              <label><input type="checkbox" id="wd-text-entry" /> Text Entry</label><br />
+              <label><input type="checkbox" id="wd-website-url" /> Website URL</label><br />
+              <label><input type="checkbox" id="wd-media-recordings" /> Media Recordings</label><br />
+              <label><input type="checkbox" id="wd-student-annotation" /> Student Annotation</label><br />
+              <label><input type="checkbox" id="wd-file-upload" /> File Uploads</label>
+            </td>
+          </tr><br /><br />
+
+          <tr>
+            <td align="right"><label htmlFor="wd-assign-to">Assign To</label></td>
+            <td><input id="wd-assign-to" defaultValue="Everyone" /></td>
+          </tr><br /><br />
+
+          <tr>
+            <td align="right"><label htmlFor="wd-due-date">Due Date</label></td>
+            <td><input id="wd-due-date" type="date" defaultValue="2025-05-17" /></td>
+          </tr>
+
+          <tr>
+            <td align="right"><label htmlFor="wd-available-from">Available From</label></td>
+            <td><input id="wd-available-from" type="date" defaultValue="2025-05-06" /></td>
+          </tr>
+
+          <tr>
+            <td align="right"><label htmlFor="wd-available-until">Available Until</label></td>
+            <td><input id="wd-available-until" type="date" defaultValue="2025-05-31" /></td>
+          </tr>
+        </tbody>
       </table>
     </div>
-);}
+  );
+}
 
--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -1,21 +1,87 @@
 export default function Assignments() {
   return (
     <div id="wd-assignments">
-      <input placeholder="Search for Assignments"
-             id="wd-search-assignment" />
+      <input placeholder="Search for Assignments" id="wd-search-assignment" />
       <button id="wd-add-assignment-group">+ Group</button>
       <button id="wd-add-assignment">+ Assignment</button>
+
+
       <h3 id="wd-assignments-title">
-        ASSIGNMENTS 40% of Total <button>+</button> </h3>
+        ASSIGNMENTS 40% of Total <button>+</button>
+      </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <a href="#/Kambaz/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
+          <a href="#/Kambaz/Courses/1234/Assignments/123" className="wd-assignment-link">
             A1 - ENV + HTML
-          </a> </li>
+          </a>
+          <div className="wd-assignment-description">
+            Multiple Modules | <strong>Not available until</strong> May 6 at 12:00am  <br />
+            <strong>Due</strong> May 13 at 11:59pm | 100pts
+          </div>         
+        </li>
         <li className="wd-assignment-list-item">
-          {/* Complete On Your Own */}
+          <a href="#/Kambaz/Courses/1234/Assignments/124" className="wd-assignment-link">
+            A2 - CSS + BOOTSTRAP
+          </a>
+          <div className="wd-assignment-description">
+            Multiple Modules | <strong>Not available until</
+            strong> May 13 at 12:00am  <br />
+            <strong>Due</strong> May 20 at 11:59pm | 100pts
+          </div>          
+        </li>
+        <li className="wd-assignment-list-item">
+          <a href="#/Kambaz/Courses/1234/Assignments/125" className="wd-assignment-link">
+            A3 - JAVASCRIPT + REACT
+          </a>
+         <div className="wd-assignment-description">
+           Multiple Modules | <strong>Not available until</
+           strong> May 20 at 12:00am  <br />
+           <strong>Due</strong> May 27 at 11:59pm | 100pts
+         </div>                    
         </li>
       </ul>
-    </div>
-);}
+
+
+    <h3 className="wd-category-title">QUIZZES 10% of Total <button>+</button></h3>
+      <ul className="wd-assignment-list">
+        <li className="wd-assignment-list-item">
+          <a href="#/Kambaz/Courses/1234/Assignments/201" className="wd-assignment-link">
+          Q1 - ENV + HTML
+          </a>
+          <div className="wd-assignment-description">
+            <strong>Not available until</strong> May 13 at 9pm | 
+            <strong> Due</strong> May 20 at 8:59pm | 
+            -/29 pts
+          </div>
+        </li>
+     </ul>
+
+
+    <h3 className="wd-category-title">EXAMS 20% of Total <button>+</button></h3>
+      <ul className="wd-assignment-list">
+        <li className="wd-assignment-list-item">
+          <a href="#/Kambaz/Courses/1234/Assignments/301" className="wd-assignment-link">
+        Midterm Exam
+          </a>
+          <div className="wd-assignment-description">
+          <strong>Not available until</strong> May 13 9pm | 
+          <strong> Due</strong> May 20 at 8:59pm | 
+          -/100 pts
+          </div>
+        </li>
+      </ul>
+
+    <h3 className="wd-category-title">PROJECT 30% of Total <button>+</button></h3>
+      <ul className="wd-assignment-list">
+        <li className="wd-assignment-list-item">
+          <a href="#/Kambaz/Courses/1234/Assignments/401" className="wd-assignment-link">
+        Final Project
+          </a>
+          <div className="wd-assignment-description">
+        <strong>Due</strong> May 12, 2025 | -/100 pts
+      </div>
+    </li>
+  </ul>
+</div>        
+  );
+}
