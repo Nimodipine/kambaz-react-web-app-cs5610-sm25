7e9c08bdfbe5a8bfba08ed7122dcf5af6adc8901
Nimodipine <megan5775@gmail.com>
2025-07-10 10:24:54 -0700

    added styling with css to lab2

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,7 +1,35 @@
+import BackgroundColors from "./Backgroundcolors";
+import Borders from "./Borders";
+import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import Float from "./Float";
+import ForegroundColors from "./ForegroundColors"
+import "./index.css"
+import Margins from "./Margins";
+import Padding from "./Padding";
+import Positions from "./Positions";
+import Zindex from "./Zindex";
+
 export default function Lab2() {
-    return (
-        <div>
-            <h2>Lab 2</h2>
-        </div>
-    );
-}
+  return (
+    <div id="wd-lab2">
+      <h2>Lab 2 - Cascading Style Sheets</h2>
+      <BackgroundColors />
+      <ForegroundColors />
+      <Borders />
+      <Padding />
+      <Margins />
+      <Corners />
+      <Dimensions />
+      <Positions />
+      <Zindex />
+      <Float />
+      <h3>Styling with the STYLE attribute</h3>
+      <p id="wd-lab2-style-attribute">
+        Style attribute allows configuring look and feel
+        right on the element. Although it's very convenient
+        it is considered bad practice and you should avoid
+        using the style attribute
+      </p>
+    </div>
+);}
