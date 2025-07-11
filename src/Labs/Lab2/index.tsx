13823623d9e20bb54268ef47126308a3bc8d1e61
Nimodipine <megan5775@gmail.com>
2025-07-11 14:30:41 -0700

    installed bootstrap and added modification

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,35 +1,47 @@
+import { Container } from "react-bootstrap";
 import BackgroundColors from "./Backgroundcolors";
 import Borders from "./Borders";
 import Corners from "./Corners";
 import Dimensions from "./Dimensions";
+import Flex from "./Flex";
 import Float from "./Float";
 import ForegroundColors from "./ForegroundColors"
+import GridLayout from "./GridLayout";
 import "./index.css"
 import Margins from "./Margins";
 import Padding from "./Padding";
 import Positions from "./Positions";
+import ReactIconsSampler from "./ReactIcons";
 import Zindex from "./Zindex";
+import BootstrapGrids from "./BootstrapGrids";
 
 export default function Lab2() {
-  return (
-    <div id="wd-lab2">
-      <h2>Lab 2 - Cascading Style Sheets</h2>
-      <BackgroundColors />
-      <ForegroundColors />
-      <Borders />
-      <Padding />
-      <Margins />
-      <Corners />
-      <Dimensions />
-      <Positions />
-      <Zindex />
-      <Float />
-      <h3>Styling with the STYLE attribute</h3>
-      <p id="wd-lab2-style-attribute">
-        Style attribute allows configuring look and feel
-        right on the element. Although it's very convenient
-        it is considered bad practice and you should avoid
-        using the style attribute
-      </p>
-    </div>
-);}
+    return (
+        <Container>
+            <div id="wd-lab2">
+                <h2>Lab 2 - Cascading Style Sheets</h2>
+                <BackgroundColors />
+                <ForegroundColors />
+                <Borders />
+                <Padding />
+                <Margins />
+                <Corners />
+                <Dimensions />
+                <Positions />
+                <Zindex />
+                <Float />
+                <GridLayout />
+                <Flex />
+                <ReactIconsSampler />
+                <BootstrapGrids />
+                <h3>Styling with the STYLE attribute</h3>
+                <p id="wd-lab2-style-attribute">
+                    Style attribute allows configuring look and feel
+                    right on the element. Although it's very convenient
+                    it is considered bad practice and you should avoid
+                    using the style attribute
+                </p>
+            </div>
+        </Container>
+    );
+}
