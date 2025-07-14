78181e382a2ad8348f879a44fbe124688dd8a7cf
Nimodipine <megan5775@gmail.com>
2025-07-13 23:23:38 -0700

    modified account navigation bar style

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,9 +1,19 @@
 import { Link } from "react-router-dom";
 export default function AccountNavigation() {
   return (
-    <div id="wd-account-navigation">
-      <Link to={`/Kambaz/Account/Signin`}  > Signin  </Link> <br/>
-      <Link to={`/Kambaz/Account/Signup`}  > Signup  </Link> <br/>
-      <Link to={`/Kambaz/Account/Profile`} > Profile </Link> <br/>
+    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+      <Link id="wd-account-signin-link"
+        to={`/Kambaz/Account/Signin`}
+        className="list-group-item active border border-0">
+        Signin  </Link>
+      <Link id="wd-account-signup-link"
+        to={`/Kambaz/Account/Signup`}
+        className="list-group-item text-danger border border-0">
+        Signup  </Link>
+      <Link id="wd-account-signup-link"
+        to={`/Kambaz/Account/Profile`}
+        className="list-group-item text-danger border border-0">
+        Profile </Link>
     </div>
-  );}
+  );
+}
