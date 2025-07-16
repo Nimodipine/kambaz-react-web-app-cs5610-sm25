d1004cb74b6fb62aec3542bef110cd7fe7706a98
Nimodipine <megan5775@gmail.com>
2025-07-16 12:47:16 -0700

    added content to lab3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,6 +1,28 @@
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import IfElse from "./IfElse";
+import LegacyFunctions from "./LegacyFunctions";
+import TemplateLiterals from "./TemplateLiterals";
+import TernaryOperator from "./TernaryOperator";
+import VariablesAndConstants
+  from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+
 export default function Lab3() {
   return (
     <div>
       <h2>Lab 3</h2>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <LegacyFunctions />
+      <TemplateLiterals />
+
+
+
     </div>
-);}
+  );
+}
