af9aad67c7e925756af9b75cc70bb1583117eb7b
Nimodipine <megan5775@gmail.com>
2025-08-08 12:28:01 -0700

    revised client.ts to fix signin issue

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -3,8 +3,6 @@ const axiosWithCredentials = axios.create({ withCredentials: true });
 
 export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
-axios.defaults.withCredentials = true;
-
 
 export const findMyCourses = async () => {
     const { data } = await axiosWithCredentials.get(`${USERS_API}/current/courses`);
